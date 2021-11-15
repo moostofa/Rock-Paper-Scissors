61f817f87ed3649fce84eb4ac17b80ff31c73236
moostofa <mmdrahinz96@gmail.com>
2021-11-15 19:46:06 +1100

    still cant get rid of bug

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const winningPairs = [
     ["Paper", "Rock"]
 ]
 
-// create RPS buttons, and wait for user to click "Begin a game" button to run the game() init function
+// create RPS buttons, and wait for user to click "Begin a game" button to run the game() function
 document.addEventListener("DOMContentLoaded", () => {
-    // create buttons for rock, paper and scissors and display it in the "options" div, which will initially be hidden
+    // create RPS buttons and display it in the "options" div, which will initially be hidden
     const btnsDiv = document.getElementById("options")
     btnsDiv.style.display = "none"
     for (let option of options) {
@@ -18,33 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
         btn.classList = "option"
         btnsDiv.append(btn)
     }
-
-    // if user clicks "start a game" button, play a round of rock, paper, scissors
+    // if user clicks "start a game" button, play a round of RPS
     document.getElementById("start").addEventListener("click", () => game())
 })
 
-// play 1 round of the game
-function playRound(playerSelection, computerSelection) {
-    // reveal the button which allows user to play another round, and hide the RPS options
-    let startButton = document.getElementById("start")
-    startButton.innerHTML = "Play another round"
-    startButton.style.display = "block"
-    document.getElementById("options").style.display = "none"
-
-    if (playerSelection === computerSelection)
-        return `It's a tie! Both you and the computer chose ${playerSelection}`
-
-    // find a win/loss pair
-    for (let pair of winningPairs) {
-        if (playerSelection === pair[0] && computerSelection === pair[1])
-            return {"winner":"You", "loser":"Computer"}
-        else if (computerSelection === pair[0] && playerSelection === pair[1])
-            return {"winner":"Computer", "loser":"You"}
-    }
-    return `There is no winner because you did not choose Rock, paper or scissors. You chose ${playerSelection}`
-}
-
-// display RPS buttons and start a game state
+// start a new game
 function game() {
     // hide start button and display the RPS buttons 
     document.getElementById("start").style.display = "none"
@@ -56,7 +34,27 @@ function game() {
         btn.addEventListener("click", () => {
             // 1st parameter: user's choice, 2nd parameter = randomly generated choice for computer
             console.log(playRound(btn.value, options[Math.floor(Math.random() * options.length)]))
-            return
         })
     })
+}
+
+// play 1 round of the game
+function playRound(playerSelection, computerSelection) {
+    // reveal the button which allows user to play another round, and hide the RPS buttons
+    let startButton = document.getElementById("start")
+    startButton.innerHTML = "Play another round"
+    startButton.style.display = "block"
+    document.getElementById("options").style.display = "none"
+
+    // check if the match is a tie
+    if (playerSelection === computerSelection)
+        return `It's a tie! Both you and the computer chose ${playerSelection}`
+
+    // find a win/loss pair
+    for (let pair of winningPairs) 
+        if (playerSelection === pair[0] && computerSelection === pair[1])
+            return {"winner":"You", "loser":"Computer"}
+        else if (computerSelection === pair[0] && playerSelection === pair[1])
+            return {"winner":"Computer", "loser":"You"}
+    return `There is no winner because you did not choose Rock, paper or scissors. You chose ${playerSelection}`
 }
