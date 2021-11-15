4615886e12c701ff3bf5ac4bfbc1c77a82b6371b
moostofa <mmdrahinz96@gmail.com>
2021-11-15 17:48:22 +1100

    start btn and options btn display behaves correctly

--- main.js
+++ main.js
@@ -6,14 +6,28 @@ const winningPairs = [
     ["Paper", "Rock"]
 ]
 
-// wait for user to click "Begin a game" button, then run the game() init function
+// create RPS buttons, and wait for user to click "Begin a game" button to run the game() init function
 document.addEventListener("DOMContentLoaded", () => {
+    // create buttons for rock, paper and scissors and display it in the "options" div, which will initially be hidden
+    const btnsDiv = document.getElementById("options")
+    btnsDiv.style.display = "none"
+    for (let option of options) {
+        let btn = document.createElement("button")
+        btn.innerHTML = option
+        btn.value = option
+        btn.classList = "option"
+        btnsDiv.append(btn)
+    }
+
+    // if user clicks "start a game" button, play a round of rock, paper, scissors
     document.getElementById("start").addEventListener("click", () => game())
 })
 
 // play 1 round of the game
 function playRound(playerSelection, computerSelection) {
     document.getElementById("start").style.display = "block"
+    document.getElementById("options").style.display = "none"
+
     if (playerSelection === computerSelection)
         return `It's a tie! Both you and the computer chose ${playerSelection}`
 
@@ -29,40 +43,16 @@ function playRound(playerSelection, computerSelection) {
     return `There is no winner because you did not choose Rock, paper or scissors. You chose ${playerSelection}`
 }
 
-// play a game - by default, a game will consist of 5 rounds
+// display RPS buttons and start a game state
 function game() {
-    // hide the "start game" button after it is clicked
     document.getElementById("start").style.display = "none"
-
-    // create buttons for rock, paper and scissors and display it in the "options" div
-    const btnsDiv = document.getElementById("options")
-    for (let option of options) {
-        let btn = document.createElement("button")
-        btn.innerHTML = option
-        btn.value = option
-        btn.classList = "option"
-        btnsDiv.append(btn)
-    }
-
-    // listen for onclick event on each of the buttons
+    // display the RPS buttons and listen for onclick event on each of the buttons
+    document.getElementById("options").style.display = "block"
     const btns = document.querySelectorAll(".option")
     btns.forEach(btn => {
         btn.addEventListener("click", () => {
+            // 1st parameter: user's choice, 2nd parameter = randomly generated choice for computer
             console.log(playRound(btn.value, options[Math.floor(Math.random() * options.length)]))
         })
     })
-    /*
-    for (let i = 1; i <= 5; i++) {
-        const playerSelection = getUserChoice()
-        const computerSelection = computerPlay()
-        
-        console.log(`Round ${i} - Player selection: ${playerSelection}`)
-        console.log(`Round ${i} - Computer selection: ${computerSelection}`)
-
-        const result = playRound(playerSelection, computerSelection)
-
-        console.log(`Round ${i} - result: ${result}`)
-        alert(result)
-    }
-    */
 }
